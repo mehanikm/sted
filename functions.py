72e70b8356f23b7ec625e7686f21022ce9d2e331
Михаил Марченко <mehanikm@MacBook-ProM.local>
2019-03-12 21:34:30 +0100

    Little improvements

--- functions.py
+++ functions.py
@@ -8,3 +8,22 @@ def opener(text_path: str):
             f"File \"{text_path}\" was not found, processing \"{text_path}\" as text.")
         text = text_path
     return text
+
+
+def assembler(text: list) -> str:
+    """Assembles list elements to string"""
+    new_text = ""
+    for ch in text:
+        new_text += ch
+    return new_text
+
+
+def spaces(text: list) -> list:
+    """Removes excessive spaces in text"""
+    c = 0
+    separators = "\"\\!?.,;:'\n) "
+    for i in range(len(text)):
+        while text[i-c] == " " and (text[i + 1-c] in separators or text[i-1-c] in " (\n"):
+            text.pop(i-c)
+            c += 1
+    return text
