0bc5176e93742ab79c040449fc3b5a99ce17b0eb
Михаил Марченко <mehanikm@MacBook-ProM.local>
2019-03-16 21:11:41 +0100

    Returns execution time at the end

--- sted.py
+++ sted.py
@@ -1,6 +1,8 @@
 import functions
 import argsparse
 
+import time
+
 args = argsparse.args()
 
 
@@ -21,26 +23,40 @@ def stedtxt(text: str, spaces=False, capitals=False, mistakes=False, statistics=
             attempt, statistic is optional, either a string or None
     """
 
+    global total_time, spaces_t, capitals_t, stats_t, assembl_t, mistake_t  # ?Time
+
     # Extract text from file/string and assing it to new [list] variable
+    opening_t = time.time()  # ?Time
     original_text = functions.opener(text)  # list
     formatted_text = original_text[:]  # list
+    opening_t -= time.time()  # ?Time
 
     # Execute function [spaces] if its flag is True
+    spaces_t = time.time()  # ?Time
     if spaces:
         formatted_text = functions.spaces(formatted_text)
+    spaces_t -= time.time()  # ?Time
 
     # Execute function [capitals] if its flag is True
+    capitals_t = time.time()  # ?Time
     if capitals:
         formatted_text = functions.capitals(formatted_text)
+    capitals_t -= time.time()  # ?Time
 
     # Execute function [statistics] if its flag is True, else assing None to variable
+    stats_t = time.time()  # ?Time
     statistic = functions.statisctics(formatted_text) if statistics else None
+    stats_t -= time.time()  # ?Time
 
     #! Assemble text back to string before return/writing to file
+    assembl_t = time.time()  # ?Time
     formatted_text = functions.assembler(formatted_text)
+    assembl_t -= time.time()  # ?Time
 
     # Execute function [mistakes] if its flag is True, else assing None to variable
+    mistake_t = time.time()  # ?Time
     mistake = functions.mistakes(formatted_text) if mistakes else None
+    mistake_t -= time.time()  # ?Time
 
     # Writing to file if filepath in [out] is specified
     if out is not None:
@@ -54,3 +70,5 @@ def stedtxt(text: str, spaces=False, capitals=False, mistakes=False, statistics=
 
 # Unpacking tuple in [print]
 print(*stedtxt(*args))
+total_time = -spaces_t - capitals_t - stats_t - assembl_t - mistake_t
+print("Time consumed:", total_time)
